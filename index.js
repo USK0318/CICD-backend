dc9bcdf9200c4260f16a5a64dc4a3ad12ffd26cb
USK0318 <saikiranuppalapati2@gmail.com>
2026-01-14 15:42:34 +0530

    second

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
